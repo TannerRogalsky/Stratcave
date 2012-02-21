4cf1853ae047a57893fc5710acb64dbd682d0fe9
Tanner Rogalsky <the.t.dawg@gmail.com>
2012-02-20 22:19:27 -0500

    We probably only need to update the physics layer (which we can now easily reference) but what the hell, it should be cheap and who know what cool stuff I might want to do.

--- screen.lua
+++ screen.lua
@@ -10,12 +10,17 @@ function Screen:initialize(jsonInTableForm)
       -- for each entry in the screen list
       for _,layerTable in ipairs(v) do
         -- make a new screen from the data and put it into level.screens
-        self.layers:insert(Layer:new(layerTable))
+        local layer = Layer:new(layerTable)
+        if layer.z == 0 then self.physicsLayer = layer end
+        self.layers:insert(layer)
       end
     else
       self[k] = v
     end
   end
+
+  -- assertions about the screen data model
+  assert(type(self.x) == "number" and type(self.y) == "number", tostring(screen).." needs layout indices.")
 
   -- finalize some values with some defaults
   self.dimensions = self.dimensions or {width = self.width or g.getWidth(), height = self.height or g.getHeight()}
