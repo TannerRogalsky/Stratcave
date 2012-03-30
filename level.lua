c9c6689a3e6666e94087ea26d046149129e9143c
Tanner Rogalsky <the.t.dawg@gmail.com>
2012-03-29 20:01:44 -0400

    Sensical player position when transitioning between screens.

--- level.lua
+++ level.lua
@@ -43,11 +43,29 @@ end
 -- @param x The screen x cooridinate to be transitioned to
 -- @param y The screen y cooridinate to be transitioned to
 -- @return Will return the screen to be transitioned to or nil if the screen with the specified indices does not exist
-function Level:transition_to_screen(x, y)
+function Level:transition_to_screen(x, y, delta_x, delta_y)
+  -- screens should probably be stored in tables with their indices indexed so we don't have to loop to search
   for _,screen in ipairs(game.current_level.screens) do
     if x == screen.x and y == screen.y then
+      local px, py = game.player.physics_body:center()
       game.current_level.current_screen:exit()
       screen:enter()
+
+      -- This stuff should maybe already be in the player character model
+      local x1,y1, x2,y2 = game.player.physics_body:bbox()
+      local p_height, p_width = x2 - x1, y2 - y1
+
+      if delta_x > 0 then
+        px = 0 + p_width
+      elseif delta_x < 0 then
+        px = g.getWidth() - p_width
+      elseif delta_y > 0 then
+        py = 0 + p_height
+      elseif delta_y < 0 then
+        py = g.getHeight() - p_height
+      end
+
+      game.player.physics_body:moveTo(px, py)
       return screen
     end
   end
