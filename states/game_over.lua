8eda57cc6e217029bcd7e1105dd241119ec4d7c0
Tanner Rogalsky <the.t.dawg@gmail.com>
2012-04-01 17:44:53 -0400

    game over screen

--- states/game_over.lua
+++ states/game_over.lua
@@ -1,8 +1,11 @@
 local Over = Game:addState('Over')
 
+local game_over_img = g.newImage("images/game_over.png")
+
 function Over:render()
   g.setColor(255,255,255)
-  g.print("Game over", 350, 280)
+  -- g.print("Game over", 350, 280)
+  g.draw(game_over_img, -200, 0)
 end
 
 function Over:update()
