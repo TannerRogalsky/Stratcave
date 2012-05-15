9a0634499e3fec03d08ff355bf1421b265964169
Tanner Rogalsky <the.t.dawg@gmail.com>
2012-05-15 12:26:57 -0400

    highscores

--- states/game_over.lua
+++ states/game_over.lua
@@ -4,6 +4,38 @@ function GameOver:enteredState()
   for i,screen in ipairs(screenshots) do
     screen:encode("screen" .. i .. ".png")
   end
+
+  local scores = {}
+  local json = require('json')
+  if love.filesystem.isFile("scores.txt") then
+    local raw = love.filesystem.read("scores.txt")
+    scores = json.decode(raw)
+  else
+    scores["easy"] = {
+      time = 0,
+      score = 0
+    }
+
+    scores["hard"] = {
+      time = 0,
+      score = 0
+    }
+
+    scores["insane"] = {
+      time = 0,
+      score = 0
+    }
+  end
+  if stats.score > scores[difficulty].score then
+    scores[difficulty].score = stats.score
+    scores[difficulty].time = math.round(stats.round_time, 1)
+    self.new_highscore = true
+  else
+    self.new_highscore = false
+  end
+
+  love.filesystem.write("scores.txt", json.encode(scores))
+
 end
 
 function GameOver:render()
@@ -11,6 +43,11 @@ function GameOver:render()
   g.print("GAME OVER. Click to return to the game menu.", 100, 100)
   g.print("Score: " .. stats.score, 100, 200)
   g.print("Survived for " .. math.round(stats.round_time, 1) .. " seconds.", 100, 300)
+
+  if self.new_highscore then
+    g.setColor(255,0,0)
+    g.print("HOLY MOTHER! NEW HIGHSCOOOOOOOORE!", 400, 200)
+  end
 end
 
 function GameOver:update(dt)
